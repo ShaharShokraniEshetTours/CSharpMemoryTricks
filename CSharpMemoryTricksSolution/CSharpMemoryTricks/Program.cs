176b2d2ac902884e5191a2e24a516d65a4843a52
shahar.shokrani <shahar.shokrani@eshet-tours.co.il>
2019-07-04 08:59:06 +0300

    First commit

---
+++ CSharpMemoryTricksSolution/CSharpMemoryTricks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpMemoryTricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
